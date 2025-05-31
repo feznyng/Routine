3a2a7bb47940c9e6502ab089007c73f1b49226c2
Ajan <ajprabakara@proton.me>
2025-05-31 10:26:22 -0400

    add browsers

--- lib/services/browser_config.dart
+++ lib/services/browser_config.dart
@@ -14,8 +14,9 @@ class BrowserData {
   final String extensionUrl;
   final String macosNmhDir;
   final String registryPath;
+  final bool macosAuto;
 
-  BrowserData({required this.windowsPaths, required this.appName, required this.windowsCommand, required this.extensionUrl, required this.macosNmhDir, required this.registryPath});
+  BrowserData({required this.windowsPaths, required this.appName, required this.windowsCommand, required this.extensionUrl, required this.macosNmhDir, required this.registryPath, required this.macosAuto});
 }
 
 final Map<Browser, BrowserData> browserData = {
@@ -25,7 +26,8 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Firefox',
       extensionUrl: 'https://addons.mozilla.org/firefox/addon/routineblocker/',
       macosNmhDir: 'Library/Application Support/Mozilla/NativeMessagingHosts/',
-      registryPath: 'SOFTWARE\\Mozilla\\NativeMessagingHosts'
+      registryPath: 'SOFTWARE\\Mozilla\\NativeMessagingHosts',
+      macosAuto: false
     ),
     Browser.chrome: BrowserData(
       windowsPaths: ['C:\\Program Files\\Google\\Chrome\\Application', 'C:\\Program Files (x86)\\Google\\Chrome\\Application'],
@@ -33,7 +35,8 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Google Chrome',
       extensionUrl: 'https://chrome.google.com/webstore/detail/routineblocker/id',
       macosNmhDir: 'Library/Application Support/Google/Chrome/NativeMessagingHosts/',
-      registryPath: 'SOFTWARE\\Google\\Chrome\\NativeMessagingHosts'
+      registryPath: 'SOFTWARE\\Google\\Chrome\\NativeMessagingHosts',
+      macosAuto: true
     ),
     Browser.edge: BrowserData(
       windowsPaths: ['C:\\Program Files (x86)\\Microsoft\\Edge\\Application', 'C:\\Program Files\\Microsoft\\Edge\\Application'],
@@ -41,7 +44,8 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Microsoft Edge',
       extensionUrl: 'https://microsoftedge.microsoft.com/addons/detail/routineblocker/id',
       macosNmhDir: 'Library/Application Support/Microsoft Edge/NativeMessagingHosts/',
-      registryPath: 'SOFTWARE\\Microsoft\\Edge\\NativeMessagingHosts'
+      registryPath: 'SOFTWARE\\Microsoft\\Edge\\NativeMessagingHosts',
+      macosAuto: true
     ),
     Browser.safari: BrowserData(
       windowsPaths: [],  // Safari is not available on Windows
@@ -49,7 +53,8 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Safari',
       extensionUrl: 'https://apps.apple.com/app/routineblocker/id',
       macosNmhDir: 'Library/Safari/NativeMessagingHosts/',
-      registryPath: ''
+      registryPath: '',
+      macosAuto: true
     ),
     Browser.opera: BrowserData(
       windowsPaths: ['C:\\Program Files\\Opera', 'C:\\Program Files (x86)\\Opera'],
@@ -57,7 +62,8 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Opera',
       extensionUrl: 'https://addons.opera.com/extensions/details/routineblocker/',
       macosNmhDir: 'Library/Application Support/com.operasoftware.Opera/NativeMessagingHosts/',
-      registryPath: 'SOFTWARE\\Opera Software\\Opera Stable\\NativeMessagingHosts'
+      registryPath: 'SOFTWARE\\Opera Software\\Opera Stable\\NativeMessagingHosts',
+      macosAuto: true
     ),
     Browser.brave: BrowserData(
       windowsPaths: ['C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application', 'C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application'],
@@ -65,6 +71,7 @@ final Map<Browser, BrowserData> browserData = {
       appName: 'Brave',
       extensionUrl: 'https://chrome.google.com/webstore/detail/routineblocker/id',  // Uses Chrome Web Store
       macosNmhDir: 'Library/Application Support/BraveSoftware/Brave-Browser/NativeMessagingHosts/',
-      registryPath: 'SOFTWARE\\BraveSoftware\\Brave-Browser\\NativeMessagingHosts'
+      registryPath: 'SOFTWARE\\BraveSoftware\\Brave-Browser\\NativeMessagingHosts',
+      macosAuto: true
     )
   };
