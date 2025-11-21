e6af39d37bbe06cfcdbf062244a142dbb4a6d74c
Ajan <ajprabakara@proton.me>
2025-11-21 10:56:38 -0500

    replace logger i with stderr in main

--- browser/native/src/main.dart
+++ browser/native/src/main.dart
@@ -167,7 +167,7 @@ void main(List<String> args) async {
       exit(1);
     },
     onDone: () {
-      logger.i('Connection to Routine app closed');
+      stderr.writeln('Connection to Routine app closed');
       exit(0);
     },
   );
